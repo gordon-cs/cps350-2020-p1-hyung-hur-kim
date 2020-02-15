cee0a818b4b569937cbafa1624650101db5615a0
glgnkim <gahngnin.kim@gordon.edu>
2020-02-14 23:56:36 -0500

    npm package date-fns added CurrentWeather View formatter/style created

--- CalendarWeather/src/WeatherApi.js
+++ CalendarWeather/src/WeatherApi.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+//import { format } from "date-fns";
 import {
   Text,
   View,
+  StyleSheet,
 } from 'react-native';
 
 /* WeatherNow component gives an overall summary of today's weather
  * at a given location, with a small preview of the next day. All its
  * data comes from props.
  */
+
+const height = '20%';
+const width = '100%';
 export default class WeatherNow extends Component {
 
   	render() {
@@ -16,7 +21,8 @@ export default class WeatherNow extends Component {
 				+ this.props.weatherData.daily.data[0].temperatureHigh + " \u2109";
 		const dateString = Date(this.props.weatherData.currently.time).toString();
     	return (
-			<View>
+			<View style={styles.screen}>
+				<View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}} >
 					<Text>
 						Wenham, MA
 					</Text>
@@ -24,14 +30,31 @@ export default class WeatherNow extends Component {
 					<Text>
 						{this.props.weatherData.currently.summary}
 					</Text>
+				</View>
+				<View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}} >
 					<Text>
 						{temp}
 					</Text>
-						<Text>
-							Today
-						</Text>
-						<Text>{todayRange}</Text>
-					</View>
+					<Text>
+						Today
+					</Text>
+					<Text>{todayRange}</Text>
+				</View>
+			</View>
     	);
   }
 }
+
+const styles = StyleSheet.create({
+	screen: {
+		width,
+		height,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'lightgrey',
+		},
+	text: {
+		fontSize: 18,
+		},
+});
+//format(dateString)
