f585e7ffa08bdeb5c31b0d2ceccbcfd9fb7af6ae
SeHee Hyung <sehee.hyung@gordon.edu>
2020-02-15 15:26:03 -0500

    Try to add icons next to event picker

--- CalendarWeather/src/EventPicker.js
+++ CalendarWeather/src/EventPicker.js
@@ -23,37 +23,11 @@ export default class EventPicker extends React.Component {
     super(props);
     
     this.state = {
-      FlatListItems: [
-        { id: '1', value: 'A' },
-        { id: '2', value: 'B' },
-        { id: '3', value: 'C' },
-        { id: '4', value: 'D' },
-        { id: '5', value: 'E' },
-        { id: '6', value: 'F' },
-        { id: '7', value: 'G' },
-        { id: '8', value: 'H' },
-        { id: '9', value: 'I' },
-        { id: '10', value: 'J' },
-        { id: '11', value: 'K' },
-        { id: '12', value: 'L' },
-        { id: '13', value: 'M' },
-        { id: '14', value: 'N' },
-        { id: '15', value: 'O' },
-        { id: '16', value: 'P' },
-        { id: '17', value: 'Q' },
-        { id: '18', value: 'R' },
-        { id: '19', value: 'S' },
-        { id: '20', value: 'T' },
-        { id: '21', value: 'U' },
-        { id: '22', value: 'V' },
-        { id: '23', value: 'W' },
-        { id: '24', value: 'X' },
-        { id: '25', value: 'Y' },
-        { id: '26', value: 'Z' },
-      ],
       allCalendars: [],
       currentDate: moment(this.props.currentDate).format("YYYY-MM-DD"),
-      allEvents: [],
+      allEvents: [{
+
+      }],
       allCalendars: calendars,
 
     };
@@ -112,11 +86,16 @@ componentDidMount() {
         sortedEvents[i] ={'start': startDate, 
                           'end': endDate, 
                           'title': allEvents[i].title, 'summary': allEvents[i].location,
-                          'color': allEvents[i].color};
+                          'color': allEvents[i].color}
         
       }
         this.setState({allEvents: sortedEvents});
   };
+
+  returnSomething() {
+    console.log("orking");
+    return (<Text>Hey</Text>);
+  }
 
   render() 
   {
@@ -124,22 +103,7 @@ componentDidMount() {
       
 
       <View style={{ flex: 1, flexDirection: "row"}}>
-        <FlatList contentContainerStyle= {{justifyContent: 'flex-start', marginLeft: 10, marginRight: 30, marginBottom: 10, marginTop: 30,}}
-          data={this.state.FlatListItems}
-          //data defined in constructor
-          ItemSeparatorComponent={this.FlatListItemSeparator}
-          //Item Separator View
-          renderItem={({ item }) => (
-            // Single Comes here which will be repeatative for the FlatListItems
-            <View style={{height: 85, marginTop: 50, marginBottom: 50, padding: 10,}}>
-              <Text>
-                {item.value}
-              </Text>
-            </View>
-          )}
-        />
         <EventCalendar
-          
           events={this.state.allEvents}
           width={width}
           initDate={this.state.currentDate}
